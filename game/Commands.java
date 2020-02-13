90847a275d6709a2121003244c54221391c1e2b4
Susannah <susannah.bennett@my.wheaton.edu>
2020-02-13 11:58:50 -0600

    Added a commands class

---
+++ game/Commands.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package game;
+
+/**
+ * @author susannahbennett
+ *
+ */
+public class Commands {
+
+	/**
+	 * 
+	 */
+	public Commands() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
