94e00d169466848f3339bfb336969562a9525a3b
Steven <steven.barker@my.wheaton.edu>
2020-02-13 12:39:07 -0600

    Modified the Item interface and Key class

--- game/Item.java
+++ game/Item.java
@@ -8,15 +8,6 @@ package game;
  *	Supertype for all items in the game
  */
 public interface Item {
-	
-	public void getItemName();
-	
-	public void pickUp();
-	
-	public void putDown();
 
-	public void use();
-	
-	
-	
+	public void function(Player p1);
 }
--- game/Key.java
+++ game/Key.java
@@ -9,48 +9,8 @@ package game;
  */
 public class Key implements Item {
 
-	private String itemname;
-	
-	/**
-	 * 
-	 */
-	public Key() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/* (non-Javadoc)
-	 * @see game.Item#pickUp()
-	 */
-	@Override
-	public void pickUp() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see game.Item#putDown()
-	 */
-	@Override
-	public void putDown() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see game.Item#putDown()
-	 */
-	@Override
-	public void getItemName() {
-		// TODO Auto-generated method stub
-
+	public void function(Player p1) {
+		
 	}
 	
-	/* (non-Javadoc)
-	 * @see game.Item#putDown()
-	 */
-	@Override
-	public void use() {
-		// TODO Auto-generated method stub
-
-	}
 }
