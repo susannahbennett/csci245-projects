8e15bdd4d46f51035eac397d12be0a86b7d8f89f
Kali <kali.grose@my.wheaton.edu>
2020-02-27 12:55:12 -0600

    Added to Map Class

--- game/Map.java
+++ game/Map.java
@@ -3,7 +3,9 @@
  */
 package game;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * @author susannahbennett
@@ -11,13 +13,16 @@ import java.util.Iterator;
  */
 public class Map implements Command {
 
+	private Scanner keyboard;
 	private Player p1;
+	private Game game;
 	
 	/**
 	 * Constructor
 	 */
 	public Map(Player p1) {
 		this.p1 = p1;
+		game = p1.getGame();
 	}
 
 	/** 
@@ -25,11 +30,28 @@ public class Map implements Command {
 	 */
 	@Override
 	public void doSomething(Player p1) {
-		Iterator<String> i = p1.returnDMap().returnMap().keySet().iterator();
+		HashMap<String, Room> seenRooms = p1.returnDMap().returnMap();
+		Iterator<String> i = seenRooms.keySet().iterator();
 		System.out.println("Rooms you've been to so far: ");
 		while (i.hasNext()) {
 			System.out.println(i.next());
 		}
+		System.out.println("Which room would you like to transport into? Otherwise type \"quit\"");
+		keyboard = new Scanner(System.in);
+		String room = keyboard.next();
+		if(!room.equals("quit")) {
+			if(seenRooms.containsKey(room)) {
+				Room nextRoom = seenRooms.get(room);
+				game.setCurrentRoom(nextRoom);
+				p1.setCurrentRoom(nextRoom);
+			}else {
+				System.out.println("Not an accessible room");
+				return;
+			}
+		}else {
+			return;
+		}
+		
 	}
 
 	/**
