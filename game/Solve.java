240724d793d3eda6d7bf20a736669c3969b73709
Susannah <susannah.bennett@my.wheaton.edu>
2020-03-04 21:38:57 -0600

    Worked on solve

--- game/Solve.java
+++ game/Solve.java
@@ -11,19 +11,22 @@ public class Solve implements Command {
 	
 	private Player p1;
 	
+	private String correctsol;
+	
 	/**
 	 * 
 	 */
 	public Solve(Player p1) {
 		this.p1 = p1;
+		correctsol = p1.getCurrentRoom().getProblem().getSolution();
 	}
 
 	/**
 	 * 	 
 	 */
 	public void doSomething(String[] command) {
-		if (command[1].equals(nextexit().getProblem().getSolution())) {
-			
+		if (command[1].equals(correctsol)) {
+			p1.getCurrentRoom().problemSolved();
 		}
 	}
 
